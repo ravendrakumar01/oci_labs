d9554227a671404b792f690e98e7b0efde3cbeb4
Ravendra <Ravendra@vvhitsolutions.com>
2026-07-05 02:34:06 +0530

    se Terraform 1.5.7 + fix backend for OCI S3 compatibility

--- backend.tf
+++ backend.tf
@@ -4,15 +4,10 @@ terraform {
    key      = "oci_labs/terraform.tfstate"
    region   = "ap-mumbai-1"
 
-   endpoints = {
-     s3 = "https://bmqtvsursobh.compat.objectstorage.ap-mumbai-1.oraclecloud.com"
-   }
-
+   endpoint                    = "https://bmqtvsursobh.compat.objectstorage.ap-mumbai-1.oraclecloud.com"
    skip_region_validation      = true
    skip_credentials_validation = true
    skip_metadata_api_check     = true
-   skip_requesting_account_id  = true
-   skip_s3_checksum            = true
-   use_path_style              = true
+   force_path_style            = true
  }
 }
