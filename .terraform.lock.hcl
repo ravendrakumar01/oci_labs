724f643dec33d8f99c611aa2b905094a37153c81
Ravendra <Ravendra@vvhitsolutions.com>
2026-07-04 22:16:52 +0530

    New Code

---
+++ .terraform.lock.hcl
@@ -0,0 +1,25 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/oracle/oci" {
+  version     = "8.19.0"
+  constraints = ">= 5.0.0"
+  hashes = [
+    "h1:+CJYg72ZrER5vF2ilr+kvpQy10hksUM0bUQrEDb+AXw=",
+    "zh:0d2df3e29ebf1d8e768559fa86f60e86293524e4e5e99cf84e7b695db63da86d",
+    "zh:295a791708c759688e5380417fad6a27531cd1315bb44b25c556f03c7b9e0d03",
+    "zh:3647850498f4f325578c1669d461aadcc169d3dde2cf58048bf09d7cc29cf2f7",
+    "zh:578c2d9498917a039bbe9ee608af897adee40a2bafeeead17e1376fee31915e3",
+    "zh:5d6cc84b1dc0fd0cfded3e5d7d8dd7215c8e85a84b43e0aa2c2f9253c20a05b6",
+    "zh:6a34be71f3d091647982c89239c45930ef2075918957924c51574d25a5de9cff",
+    "zh:85e0e0273dd3f24a0f1b40d53b0c28a4767c5c193a0fc0f580302c79b498a34d",
+    "zh:95f14a63e63d80fda595a16588890079f0d3813c9efd25a9cbbb0d2c9f0ed54c",
+    "zh:9b12af85486a96aedd8d7984b0ff811a4b42e3d88dad1a3fb4c0b580d04fa425",
+    "zh:9b98b31b75db4f77b7674d739b36c6a1835f74179fd08946478f16f21f38d1fd",
+    "zh:b36ecc420be9f70da3f0b8e1f817d41876bf3bc6343747e0f397e88854e0c71d",
+    "zh:b6aa3f2085e5d8b8ad9808cb75c7a69cb392ca56d7f789ba375a9d057b093657",
+    "zh:cb52be156360270e42bba86196412b0393a1dd8610ffb17299c762827509e04f",
+    "zh:da9e16acb0c729f65319f2c7c754613c2d99407bca86c2dfdfa1701c1ebb08ca",
+    "zh:ed22df97b56077cdac4aa909db24be858755eefac7b0a68415411429fbf805c4",
+  ]
+}
---
+++ main.tf
@@ -0,0 +1,173 @@
+## --- PROD-COMPARTMENT ---##
+
+resource "oci_identity_compartment" "prod_compartment" {
+  compartment_id = var.tenancy_ocid
+  name = "PRODUCTION"
+  description = "we will use this compartment for production workload only"
+enable_delete = true
+
+}
+
+##--- PROD-VCN ----##
+
+resource "oci_core_vcn" "prod_vcn" {
+  compartment_id = oci_identity_compartment.prod_compartment.id
+  cidr_blocks =["172.15.0.0/16"]
+  display_name = "PROD_VCN"
+  dns_label = "prodvcn"
+  
+}
+
+##---- RPOD-SUBNET  ----##
+
+resource "oci_core_subnet" "prod_subnet" {
+  compartment_id = oci_identity_compartment.prod_compartment.id
+  vcn_id = oci_core_vcn.prod_vcn.id
+  cidr_block = "172.15.1.0/24"
+  display_name = "PROD_SUBNET"
+  dns_label = "prodsubnet"
+}
+
+## ---  DEV-COMPARTMENT  ----##
+resource "oci_identity_compartment" "dev_compartment" {
+  compartment_id = var.tenancy_ocid
+  name = "DEV"
+  description = "we will use this compartment for dev workload only"
+enable_delete = true
+
+}
+
+### --- DEV-VCN -----##
+
+resource "oci_core_vcn" "dev_vcn" {
+  compartment_id = oci_identity_compartment.dev_compartment.id
+  cidr_blocks = ["172.17.0.0/16"]
+  display_name = "DEV_VCN"
+  dns_label = "devvcn"
+}
+
+####----- DEV-SUBNET  ------##
+
+resource "oci_core_subnet" "dev_subnet" {
+  compartment_id = oci_identity_compartment.dev_compartment.id
+  vcn_id = oci_core_vcn.dev_vcn.id
+  cidr_block = "172.17.1.0/24"
+  display_name = "DEV_SUBNET"
+  dns_label = "devsubnet"
+  
+}
+
+### --- TEST-COMPARTMENT  ---##
+
+resource "oci_identity_compartment" "test_compartment" {
+  compartment_id = var.tenancy_ocid
+  name = "TEST"
+  description = "we will use this compartment for test workload only"
+enable_delete = true
+
+}
+
+## --- TEST-VCN ---  ###
+resource "oci_core_vcn" "test_vcn" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  cidr_blocks    = ["172.16.0.0/16"]
+  display_name   = "TEST_VCN"
+  dns_label      = "testvcn"
+}
+
+## --- TEST-Pub-Subnet --- ##
+resource "oci_core_subnet" "test_pub_subnet" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id         = oci_core_vcn.test_vcn.id
+  cidr_block     = "172.16.1.0/24"
+  display_name   = "TEST_PUB_SUBNET"
+  dns_label      = "testpubsubnet"
+  route_table_id = oci_core_route_table.test_pub_rt.id
+  security_list_ids = [oci_core_security_list.test_sl.id]
+  prohibit_public_ip_on_vnic = false
+}
+
+## --- TEST-Priv-Subnet --- ##
+resource "oci_core_subnet" "test_priv_subnet" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  cidr_block = "172.16.2.0/24"
+  display_name = "TEST_PRIV_SUBNET"
+  dns_label = "testprivsubnet"
+  route_table_id = oci_core_route_table.test_priv_rt.id
+  security_list_ids = [oci_core_security_list.test_sl.id]
+  prohibit_public_ip_on_vnic = true
+
+}
+
+# Internet Gateway (for public subnet)
+resource "oci_core_internet_gateway" "test_ig" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id         = oci_core_vcn.test_vcn.id
+  display_name   = "TEST_IG"
+  enabled        = true
+}
+
+##  --  TEST-NAT Gateway  ---###
+
+resource "oci_core_nat_gateway" "test_nat" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  display_name = "TEST_NAT"
+    
+}
+
+##--  TEST-PUB-RT  ----###
+
+resource "oci_core_route_table" "test_pub_rt" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  display_name = "TEST_PUB_RT"
+
+  route_rules {
+    destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.test_ig.id
+  }
+  
+}
+
+### ---  TEST-PRI-RT ----##
+
+resource "oci_core_route_table" "test_priv_rt" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  display_name = "TEST_PRIV_RT"
+
+  route_rules {
+    destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    network_entity_id = oci_core_nat_gateway.test_nat.id
+    
+  }
+  
+}
+
+
+resource "oci_core_security_list" "test_sl" {
+  compartment_id = oci_identity_compartment.test_compartment.id
+  vcn_id = oci_core_vcn.test_vcn.id
+  display_name = "TEST_SL"
+
+  egress_security_rules {
+    destination = "0.0.0.0/0"
+    protocol = "all"
+
+  }
+
+  ingress_security_rules {
+    source = "0.0.0.0/0"
+    protocol = "TCP"
+    tcp_options {
+      min = 22
+      max = 22
+
+    }
+  }
+
+}
