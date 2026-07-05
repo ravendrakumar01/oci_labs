991d2cb4ce8430fe76120ae54bd8605176fda4d8
Ravendra <Ravendra@vvhitsolutions.com>
2026-07-05 11:18:13 +0530

    Add resource changes detail to email notifications

--- main.tf
+++ main.tf
@@ -170,4 +170,14 @@ resource "oci_core_security_list" "test_sl" {
     }
   }
 
+    ingress_security_rules {
+    source = "0.0.0.0/0"
+    protocol = "6"
+    tcp_options {
+      min = 3389
+      max = 3389
+
+    }
+  }
+
 }
